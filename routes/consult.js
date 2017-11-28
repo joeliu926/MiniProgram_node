e6495f98d2e8c508dbcd1cba92d450eff8702c20
yongchao71 <yongchao71@163.com>
2017-11-28 18:30:00 +0800

    add consult customer

--- routes/consult.js
+++ routes/consult.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var consultServer =require('../service/consultServer');
+/**
+ * 添加会话信息
+ */
 router.post('/addconsultation', function(req, res, next) {
     consultServer.addconsultation(req, res, next);
 });
+/**
+ * 获取咨询列表
+ */
 router.post('/list', function(req, res, next) {
     consultServer.consultationlist(req, res, next);
 });
+/**
+ * 获取咨询轨迹
+ */
 router.post('/trail', function(req, res, next) {
     consultServer.trail(req, res, next);
 });
+
+/**
+ * 获取咨询项目
+ */
+router.post('/consultitems', function(req, res, next) {
+    consultServer.consultitems(req, res, next);
+});
+/**
+ * 获取一个咨询下面的所有客户
+ */
+router.post('/consultcustomers', function(req, res, next) {
+    consultServer.consultcustomers(req, res, next);
+});
+
 module.exports = router;
--- service/consultServer.js
+++ service/consultServer.js
@@ -5,7 +5,9 @@ var defualtCfg={
     url:CONSTANT.remoteHost+":"+CONSTANT.remotePort+'/api/consultation',
     contentType:'application/json'
 };
-
+/**
+ * 添加会话信息
+ */
 function addconsultation(req, res, next){
     defualtCfg.method="POST";
     var opt=appUtil.extend({},defualtCfg);
@@ -26,7 +28,9 @@ function addconsultation(req, res, next){
     httpClient(opt);
     //res.send({'aaa':'aaaa'});
 }
-
+/**
+ * 获取咨询列表
+ */
 function consultationlist(req, res, next){
     defualtCfg.method="POST";
     var opt=appUtil.extend({},defualtCfg);
@@ -48,6 +52,9 @@ function consultationlist(req, res, next){
     httpClient(opt);
     //res.send({'aaa':'aaaa'});
 }
+/**
+ * 获取咨询轨迹
+ */
 function trail(req, res, next){
     defualtCfg.method="get";
     var opt=appUtil.extend({},defualtCfg);
@@ -68,8 +75,55 @@ function trail(req, res, next){
     httpClient(opt);
     //res.send({'aaa':'aaaa'});
 }
+/**
+ * 获取咨询项目/api/consultation/{会话ID}/product/{客户unionid}
+ */
+function consultitems(req, res, next){
+    defualtCfg.method="get";
+    var opt=appUtil.extend({},defualtCfg);
+    opt.url+="/"+req.body.id+"/product/"+req.body.customerUnionId;
+    console.log(opt.url);
+    opt.callBack=function(error, response, body){
+        if(error)
+        {
+            res.send(error);
+        }
+        else {
+            console.log(JSON.parse(body));
+            res.send(JSON.parse(body));
+        }
+    };
+    httpClient(opt);
+}
+/**
+ * 获取一个会话下的咨询客户/api/consultation/{id}/customers/list?wxNickname='uyuu'&fieldValue=1
+ * id 咨询会话id
+ * wxNickName 客户昵称
+ * fieldValue 1最近 2最热
+ */
+function consultcustomers(req, res, next){
+    defualtCfg.method="get";
+    var opt=appUtil.extend({},defualtCfg);
+    opt.url+="/"+req.body.id+"/customers/list?wxNickname="+req.body.wxNickname+"&fieldValue="+req.body.fieldValue;
+    console.log(opt.url);
+    opt.callBack=function(error, response, body){
+        if(error)
+        {
+            res.send(error);
+        }
+        else {
+            console.log(JSON.parse(body));
+            res.send(JSON.parse(body));
+        }
+    };
+    httpClient(opt);
+}
+
+
 module.exports = {
     addconsultation: addconsultation,
     consultationlist:consultationlist,
-    trail:trail
+    trail:trail,
+    consultitems:consultitems,
+    consultcustomers:consultcustomers
 }
