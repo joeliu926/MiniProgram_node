ab90c9bb5257e0080ffab01622eb184171020763
yongchao71 <yongchao71@163.com>
2017-12-05 12:48:32 +0800

    change customer

--- routes/customer.js
+++ routes/customer.js
@@ -4,4 +4,10 @@ var customerServer =require('../service/customerServer');
 router.post('/addcustomer', function(req, res, next) {
     customerServer.addcustomer(req, res, next);
 });
+router.post('/getcustomer', function(req, res, next) {
+    customerServer.getcustomer(req, res, next);
+});
+router.post('/update', function(req, res, next) {
+    customerServer.update(req, res, next);
+});
 module.exports = router;
--- service/customerServer.js
+++ service/customerServer.js
@@ -5,7 +5,12 @@ var defualtCfg={
     url:CONSTANT.remoteHost+":"+CONSTANT.remotePort+'/api/customer/',
     contentType:'application/json'
 };
-
+/**
+ * 添加客户
+ * @param req
+ * @param res
+ * @param next
+ */
 function addcustomer(req, res, next){
     defualtCfg.method="POST";
     var opt=appUtil.extend({},defualtCfg);
@@ -27,7 +32,62 @@ function addcustomer(req, res, next){
 
     //res.send({'aaa':'aaaa'});
 }
+/**
+ * 获取客户信息 api/customer/customerDetail/{customerId}
+ * @param req
+ * @param res
+ * @param next
+ */
+function getcustomer(req, res, next){
+    defualtCfg.method="GET";
+    var opt=appUtil.extend({},defualtCfg);
+    opt.url+="/"+req.body.customerId;
+    //opt.url+="list?unionid="+req.body.unionId+"&caseIds="+req.body.caseIds+"&productCode="+req.body.productCode;
+    //opt.data=req.body;
+    console.log(opt.url);
+    opt.callBack=function(error, response, body){
+        if(error)
+        {
+            res.send(error);
+        }
+        else {
+            console.log(JSON.parse(body));
+            res.send(JSON.parse(body));
+        }
+    };
+    httpClient(opt);
 
+    //res.send({'aaa':'aaaa'});
+}
+/**
+ * 更新客户 api/customer/update
+ * @param req
+ * @param res
+ * @param next
+ */
+function update(req, res, next){
+    defualtCfg.method="POST";
+    var opt=appUtil.extend({},defualtCfg);
+    opt.url+="update";
+    //opt.url+="list?unionid="+req.body.unionId+"&caseIds="+req.body.caseIds+"&productCode="+req.body.productCode;
+    opt.data=req.body;
+    console.log(opt.url);
+    opt.callBack=function(error, response, body){
+        if(error)
+        {
+            res.send(error);
+        }
+        else {
+            console.log(JSON.parse(body));
+            res.send(JSON.parse(body));
+        }
+    };
+    httpClient(opt);
+
+    //res.send({'aaa':'aaaa'});
+}
 module.exports = {
     addcustomer: addcustomer,
+    getcustomer:getcustomer,
+    update:update
 }
