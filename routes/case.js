f3106ca1d12684557164efcea0fe9e24d775eb49
yiweidujiang <yiweidujiang869527@163.com>
2018-01-08 13:45:52 +0800

    duoliebiao

--- routes/case.js
+++ routes/case.js
@@ -5,6 +5,11 @@ var caseServer =require('../service/caseServer');
 router.post('/list', function(req, res, next) {
     caseServer.list(req, res, next);
 });
+router.post('/morelist', function(req, res, next) {
+    caseServer.morelist(req, res, next);
+});
+
+
 router.post('/detail', function(req, res, next) {
     caseServer.detail(req, res, next);
 });
--- service/caseServer.js
+++ service/caseServer.js
@@ -12,7 +12,7 @@ function list(req, res, next){
     defualtCfg.method="get";
     var opt=appUtil.extend({},defualtCfg);
     //opt.url+="list/"+req.body.unionId;
-    opt.url+="list?unionid="+req.body.unionId+"&caseIds="+req.body.caseIds+"&productCode="+req.body.productCode;
+    opt.url+="list?unionid="+req.body.unionId+"&caseIds="+req.body.caseIds+"&productCodes="+req.body.productCodes;
 
     console.log(opt.url);
     opt.callBack=function(error, response, body){
@@ -30,6 +30,35 @@ function list(req, res, next){
     //res.send({'aaa':'aaaa'});
 
 }
+
+/*多案例*/
+function morelist(req, res, next){
+
+
+    defualtCfg.method="POST";
+    var opt=appUtil.extend({},defualtCfg);
+     opt.data=req.body;
+    //opt.url+="list/"+req.body.unionId;"&caseIds="+req.body.caseIds+
+    opt.url+="listByProducts";
+
+    console.log(opt.url);
+    opt.callBack=function(error, response, body){
+        if(error)
+        {
+            res.send(error);
+        }
+        else {
+            //console.log(JSON.parse(body));
+            res.send(JSON.parse(body));
+        }
+    };
+    httpClient(opt);
+
+    //res.send({'aaa':'aaaa'});
+
+}
+
+
 function detail(req,res,next){
     defualtCfg.method="get";
     var opt=appUtil.extend({},defualtCfg);
@@ -51,5 +80,6 @@ function detail(req,res,next){
 }
 module.exports = {
     list: list,
-    detail:detail
+    detail:detail,
+    morelist:morelist
 }
