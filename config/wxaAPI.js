91fedd90eb506ca0f433af0bbf9a87b5671356d2
yongchao71 <yongchao71@163.com>
2018-01-17 09:08:41 +0800

    add qrcode

--- config/wxaAPI.js
+++ config/wxaAPI.js
@@ -1,7 +1,7 @@
 var wxConfig=require('./wx_config');
 var config={
     authorize:{
-        authorizecode:{
+        authorizecode:{ //微信开发平台授权
             method:"POST",
             url:"https://api.weixin.qq.com/sns/jscode2session?appid="+wxConfig.appid+"&secret="+wxConfig.appSecret+"&js_code=_CODE_&grant_type=authorization_code"
         },
@@ -10,7 +10,21 @@ var config={
             url:"https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code"
         }
     },
-    appid:wxConfig.appid
+    appid:wxConfig.appid,
+    wxSendMessage:{
+        method:"POST",
+        url:wxConfig.route+"/wx/msg/sendmessage" //发送微信客服消息
+    },
+    wxa:{
+        getAccessToken:{
+            method:"GET",
+            url:`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${wxConfig.appid}&secret=${wxConfig.appSecret}`//获取token
+        },
+        getQrCode:{
+            method:"POST",
+            url:`https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=_ACCESSTOKEN_`
+        }
+    }
 
 };
 module.exports=config;
