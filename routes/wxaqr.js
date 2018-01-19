7fdd5118cc40cb7b64de1d4f652cee20c6a3f9fc
yongchao71 <yongchao71@163.com>
2018-01-19 17:27:16 +0800

    add qrcode

--- routes/wxaqr.js
+++ routes/wxaqr.js
@@ -3,6 +3,6 @@ var express = require('express');
 var router = express.Router();
 var wxaqrServer =require('../service/wxaqrServer');
 router.post('/genwxaqrcode', function(req, res, next) {
-    clueServer.genwxaqrcode(req, res, next);
+    wxaqrServer.genwxaqrcode(req, res, next);
 });
 module.exports = router;
--- service/wxaqrServer.js
+++ service/wxaqrServer.js
@@ -2,6 +2,7 @@ var CONSTANT=require('../config/constant');
 var httpClient=require('../utils/httpClient');
 var appUtil=require('../utils/appUtils');
 var loger=require("./../utils/loger");
+var baseServer=require('./baseServer');
 var defualtCfg={
     url:CONSTANT.remoteHost+":"+CONSTANT.remotePort+'/api/wxaqr',
     contentType:'application/json'
@@ -13,26 +14,35 @@ var defualtCfg={
  * @param next
  */
 function genwxaqrcode(req, res, next){
-    defualtCfg.method="POST";
-    var opt=appUtil.extend({},defualtCfg);
-    let sessionId=req.body.sessionId;
-    let customerId=req.body.customerId;
-    opt.url+=`/genwxaqrcode`;
-    // opt.url+="list?unionid="+req.body.unionId+"&caseIds="+req.body.caseIds+"&productCode="+req.body.productCode;
-    opt.data=req.body;
-    loger.info(opt.url);
-    opt.callBack=function(error, response, body){
-        if(error)
-        {
-            loger.error("get qrcode error-----",opt.url);
-            res.send(error);
-        }
-        else {
-            loger.info("get clue detail====>",JSON.parse(body));
-            res.send(JSON.parse(body));
-        }
-    };
-    httpClient(opt);
+
+
+    baseServer.updateToken(function (accessToken) {
+
+        defualtCfg.method="POST";
+        var opt=appUtil.extend({},defualtCfg);
+        let sessionId=req.body.sessionId;
+        let customerId=req.body.customerId;
+        opt.url+=`/genwxaqrcode`;
+        let postData={
+            wxaqrData:req.body,
+            access_token:accessToken
+        };
+        opt.data=postData;
+        loger.info(opt.url);
+        opt.callBack=function(error, response, body){
+            if(error)
+            {
+                loger.error("get qrcode error-----",opt.url);
+                res.send(error);
+            }
+            else {
+                loger.info("genwxaqrcode====>",JSON.parse(body));
+                res.send(JSON.parse(body));
+            }
+        };
+        httpClient(opt);
+
+    });
 
 }
 
